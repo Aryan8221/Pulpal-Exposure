2c59456ba51d45a000e49a91d10f1c115e08cf85
Aryan Neizehbaz <aneizehbaz@gmail.com>
2025-08-22 11:53:08 +0330

    .

--- Pretrain/run.sh
+++ Pretrain/run.sh
@@ -1,6 +1,6 @@
 python3 main.py \
-    --data_dir=../. \
-    --json_list=../DENTAL/jsons \
+    --data_dir=../../. \
+    --json_list=../../DENTAL/jsons \
     --spatial_dims=2 \
     --in_channels=3
 
